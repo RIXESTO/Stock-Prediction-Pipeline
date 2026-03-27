68cb5ac5e68add67b18c9af62a36db4337114ff1
Tushar Sharma <tusharsharma@Tushars-MacBook-Air.local>
2026-03-27 13:27:57 +0530

    fix: use custom session and update yfinance to fix chrome impersonation error

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def calculate_rsi(data, window=14):
     return 100 - (100 / (1 + rs))
 
 @st.cache_data(ttl=3600)
-def fetch_and_preprocess(ticker, scaler):
+def fetch_and_preprocess(ticker, _scaler):
     tk = yf.Ticker(ticker)
     data = tk.history(period="1y")
     
